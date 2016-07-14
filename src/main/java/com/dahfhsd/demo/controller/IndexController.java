ef03bf9665674f6939aa7457611d311eb04e4d6d
dhlongsir <2405902561@qq.com>
2016-07-14 20:10:47 +0800

    增加indexControl

---
+++ src/main/java/com/dahfhsd/demo/controller/IndexController.java
@@ -0,0 +1,11 @@
+package com.dahfhsd.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by lenovo on 2016/7/14.
+ */
+@Controller
+public class IndexController {
+    //index
+}
